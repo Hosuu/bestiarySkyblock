66097f0c5853ab02cef2237b01ef13cbfdeeca46
Mateusz Siller <mateusz.siller@gmail.com>
2023-10-03 18:33:51 +0200

    awaitfix

--- src/main.ts
+++ src/main.ts
@@ -1,55 +1,60 @@
 import { BESTIARY, BESTIARY_BRACKETS } from './bestiary'
 import './style.css'
 
-const playerName = prompt('Enter username')
+;(async () => {
+	const playerName = prompt('Enter username')
 
-const response = await fetch(`https://sky.shiiyu.moe/api/v2/profile/${playerName}`)
-const data = await response.json()
-//@ts-ignore
-const bestiaryData = Object.values(data.profiles).find((p) => p.current === true).raw.bestiary.kills
+	const response = await fetch(`https://sky.shiiyu.moe/api/v2/profile/${playerName}`)
+	const data = await response.json()
+	//@ts-ignore
+	const bestiaryData = Object.values(data.profiles).find((p) => p.current === true).raw.bestiary
+		.kills
 
-interface MobEntry {
-	category: string
-	name: string
-	kills: number
-	nextMilestone: number
-	toNextMilestone: number
-}
-
-const MobEntries = []
-
-for (const category in BESTIARY) {
-	if (category === 'fishing') continue
-
-	for (const { mobs, bracket, cap, name } of BESTIARY[category].mobs) {
-		let kills = 0
-		for (const mobId of mobs) kills += bestiaryData[mobId] ?? 0
-
-		let nextMilestone = 0
-		for (const milestone of BESTIARY_BRACKETS[bracket]) {
-			if (milestone <= kills) continue
-			nextMilestone = milestone
-			break
-		}
-
-		let toNextMilestone = nextMilestone - kills
-		if (kills >= cap) toNextMilestone = -1
-
-		const mobEntry: MobEntry = {
-			//@ts-ignore
-			category: BESTIARY[category].name,
-			name,
-			kills,
-			nextMilestone,
-			toNextMilestone,
-		}
-		MobEntries.push(mobEntry)
+	interface MobEntry {
+		category: string
+		name: string
+		kills: number
+		nextMilestone: number
+		toNextMilestone: number
 	}
-}
 
-const sortedEntreis = MobEntries.filter((e) => e.toNextMilestone >= 0).sort(
-	(a, b) => a.toNextMilestone - b.toNextMilestone
-)
+	const MobEntries = []
 
-for (const { name, category, toNextMilestone } of sortedEntreis)
-	document.write(`${name} @ ${category} in ${toNextMilestone} kills </br>`)
+	for (const category in BESTIARY) {
+		if (category === 'fishing') continue
+
+		//@ts-ignore
+		for (const { mobs, bracket, cap, name } of BESTIARY[category].mobs) {
+			let kills = 0
+			for (const mobId of mobs) kills += bestiaryData[mobId] ?? 0
+
+			let nextMilestone = 0
+			//@ts-ignore
+			for (const milestone of BESTIARY_BRACKETS[bracket]) {
+				if (milestone <= kills) continue
+				nextMilestone = milestone
+				break
+			}
+
+			let toNextMilestone = nextMilestone - kills
+			if (kills >= cap) toNextMilestone = -1
+
+			const mobEntry: MobEntry = {
+				//@ts-ignore
+				category: BESTIARY[category].name,
+				name,
+				kills,
+				nextMilestone,
+				toNextMilestone,
+			}
+			MobEntries.push(mobEntry)
+		}
+	}
+
+	const sortedEntreis = MobEntries.filter((e) => e.toNextMilestone >= 0).sort(
+		(a, b) => a.toNextMilestone - b.toNextMilestone
+	)
+
+	for (const { name, category, toNextMilestone } of sortedEntreis)
+		document.write(`${name} @ ${category} in ${toNextMilestone} kills </br>`)
+})()
